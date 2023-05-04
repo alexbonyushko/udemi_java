83a00cf8bbbf217f33f0adce63a1ccc0ca8b705b
alboniushko <a.bonyushko@gmail.com>
2023-05-04 21:05:04 +0200

    check

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import static jdk.internal.org.jline.utils.Colors.s;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(s);
+        /*check*/
+        String s="Hello world!";
     }
 }
