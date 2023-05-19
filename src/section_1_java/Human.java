75b87c9e32b906e95bdff58d14df27d0e5ee02b2
alboniushko <a.bonyushko@gmail.com>
2023-05-19 19:45:19 +0200

    15.Overload

---
+++ src/section_1_java/Human.java
@@ -0,0 +1,14 @@
+package section_1_java;
+
+public class Human {
+    public Human() {
+    }
+
+    public Human(int q) {
+        System.out.println(q);
+    }
+
+    public Human(String q) {
+        System.out.println(q);
+    }
+}
--- src/section_1_java/Main.java
+++ src/section_1_java/Main.java
@@ -1,6 +1,8 @@
 package section_1_java;
 
-
+/**
+ * 15.Overload
+ */
 public class Main {
     public static void main(String[] args) {
         Computer apple = new Computer(1000, 1500, "MacBook");
@@ -8,5 +10,7 @@ public class Main {
 
         Cow burenka = new Cow("Muuu", 28);
         burenka.mu();
+
+        Human human = new Human();
     }
 }
